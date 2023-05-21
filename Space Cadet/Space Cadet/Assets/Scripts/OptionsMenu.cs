e678e48f73a3a05868ec94c1e94006825fcf65c1
BigZeam <mpsportsguy13@gmail.com>
2023-05-21 06:55:52 -0400

    main and pause updated

---
+++ Space Cadet/Space Cadet/Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using TMPro;
+
+public class OptionsMenu : MonoBehaviour
+{
+
+    public AudioMixer mixer;
+    Resolution[] resolutions;
+    public TMP_Dropdown resolutionDropdown;
+
+    private void Start() {
+        resolutions = Screen.resolutions;    
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+
+        int i = 0;
+        foreach (Resolution r in resolutions)
+        {
+            string option = r.width + " x " + r.height;
+
+            options.Add(option);
+            
+            if( r.width == Screen.currentResolution.width && r.height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+
+            i++;
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = resolutions.Length;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetVolume(float volume)
+    {
+        mixer.SetFloat("volume", volume);
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullScreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution res = resolutions[resolutionIndex];
+
+        Screen.SetResolution(res.width, res.height, Screen.fullScreen);
+    }
+
+
+}
